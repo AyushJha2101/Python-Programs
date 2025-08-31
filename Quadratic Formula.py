1062b528ca93c187a9e92c8e73530ffd618d196d
Ayush Jha <138970149+AyushJha2101@users.noreply.github.com>
2025-08-31 16:51:11 +0530

    Add files via upload

---
+++ Quadratic Formula.py
@@ -0,0 +1,10 @@
+b= float(input("Enter the value of b: "))
+a= float(input("Enter the value of a: "))
+c= float(input("Enter the value of c: "))
+quadractic_frmla= b**2-4*a*c
+if quadractic_frmla<0:
+    print("No real solution")
+elif quadractic_frmla==0:
+    print("One real solution: {root}")
+else:
+    print("No real solution")
